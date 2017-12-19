302f912a69ddb59d8fb9fa611f586e4498ce1fa6
Aman R <aman.btech12@gmail.com>
2017-12-19 11:39:07 +0530

    Added Styles

--- public/web/js/script.js
+++ public/web/js/script.js
@@ -2,11 +2,11 @@ $(function() {
     if (typeof messages.icon != 'undefined' && messages.icon != '') {
         console.log(messages);
         $.toast({
-            heading: messages.title,
+            heading: 'Ca',
             text: messages.message,
             position: 'bottom-right',
             loaderBg: '#ff6849',
-            icon: messages.icon,
+            icon: 'error',
             hideAfter: 3500,
             stack: 6
         });
