9f0eddd7c676c6bbd3ce8056e500cdbab91c2145
Aman R <aman.btech12@gmail.com>
2018-01-04 09:48:29 +0530

    Layouts

--- controllers/front/SiteController.js
+++ controllers/front/SiteController.js
@@ -7,6 +7,15 @@ var SiteController = {
     },
     about: function(req, res) {
         res.render('front/site/about');
+    },
+    login: function(req, res) {
+        res.render('front/site/login');
+    },
+    tutorRegistration: function(req, res) {
+        res.render('front/site/tutor-registration');
+    },
+    studentRegistration: function(req, res) {
+        res.render('front/site/student-registration');
     }
 }
 
--- routes/front/site.js
+++ routes/front/site.js
@@ -6,4 +6,8 @@ var SiteController = require('../../controllers/front/SiteController');
 router.get('/', SiteController.index);
 router.get('/about', SiteController.about);
 
+router.get('/log-in', SiteController.login);
+router.get('/tutor-registration', SiteController.tutorRegistration);
+router.get('/student-registration', SiteController.studentRegistration);
+
 module.exports = router;
